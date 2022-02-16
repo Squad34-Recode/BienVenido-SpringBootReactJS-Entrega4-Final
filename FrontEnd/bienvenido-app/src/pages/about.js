84142c7ebebab3a5547bfa2f208ce89ddfad48c4
CJBiohacker <cj.moyses@gmail.com>
2022-02-16 18:33:50 -0300

    feat : páginas componentizadas e index.html

---
+++ FrontEnd/bienvenido-app/src/pages/about.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const about = () =>{
+    return (
+        <h1>about</h1>
+    )
+}
+export default about;
---
+++ FrontEnd/bienvenido-app/src/pages/contact.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const contact = ()=>{
+    return (
+        <h1>teste</h1>
+    )
+}
+export default contact;
